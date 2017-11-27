5287bba85c2ce8c69c79e8ba2c4590add3b56171
phalax4 <jeremylim995@gmail.com>
2017-11-26 18:09:51 -0600

    Switch github pages

--- src/app/visuals/shared/barchartdemo.component.ts
+++ src/app/visuals/shared/barchartdemo.component.ts
@@ -8,31 +8,49 @@ import {BaseChartDirective} from "ng2-charts";
 })
 
 export class BarChartDemoComponent {
-  public barChartOptions:any = {
-    maintainAspectRatio: true,
+  public lineChartOptions: any = {
+   // maintainAspectRatio: true,
     responsive: true,
   };
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective;
 
-  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartColors:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend:boolean = true;
+  public lineChartType:string = 'line';
   public barChartHasRendered: number = 1;
 
-  public barChartData: any[] =  [
+  public lineChartData: Array<any> =  [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
   ];
-
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
 
   public randomize(): void {
     if (this.chart !== undefined && (this.barChartHasRendered <= 3)) {
