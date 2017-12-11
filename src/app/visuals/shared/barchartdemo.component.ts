0fb48624d0c09564515f5b260cf7a43af36a3173
phalax4 <jeremylim995@gmail.com>
2017-12-11 17:04:14 -0600

    Fix graph for Zipfs law

--- src/app/visuals/shared/barchartdemo.component.ts
+++ src/app/visuals/shared/barchartdemo.component.ts
@@ -15,7 +15,7 @@ export class BarChartDemoComponent {
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective;
 
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['to', 'the', 'and', 'of', 'i', 'a', 'it', 'her', 'was', 'she', 'in', 'not', 'you', 'be', 'that', 'he', 'had', 'but', 'as', 'for', 'have', 'is', 'with', 'very', 'mr', 'his', 'at', 'so', 's', 'emma', 'all', 'could', 'would', 'been', 'him', 'no', 'my', 'mrs', 'on', 'any', 'do', 'were', 'miss', 'me', 'by', 'will', 'must', 'which', 'there', 'from', 'they', 'what', 'this', 'harriet', 'or', 'such', 'much', 'if', 'said', 'more', 'an', 'are', 'one', 'weston', 'every', 'them', 'am', 'than', 'well', 'thing', 'knightley', 'elton', 'think', 'how', 'should', 'your', 'when', 'little', 'being', 'never', 'good', 'did', 'we', 'only', 'know', 'might', 'woodhouse', 'say', 'now', 'their', 'jane', 'own', 'who', 'can', 'quite', 'herself', 'time', 'great', 'some', 'nothing', 'too', 'before', 'has', 'about', 'most', 'dear', 'fairfax', 'always', 'man', '?"', 'thought', 'soon', 'churchill', 'see', 'other', 'may', 'again', 'shall', 'without', 'out', 'first', 'frank', 'father', 'sure', 'indeed', 'like', 'made', 'body', 'ever', 'oh', 'day', 'young', 'up', 'two', 'friend', 'though', 'better', 'come', 'then', 'just', 'into', 'after', 'hartfield', 'give', 'upon', 'way', 'here', 'really', 'make', 'bates', 'having', 'long', 'rather', 'himself', 'us', 'hope', 'done', 'cannot', 'seemed', 'over', 'away', 'many', 'poor', 'wish', 'while', 'even', 'go', 'woman', 'however', 'home', 'does', 'enough', 'mind', 'happy', 'highbury', 'yes', 'its', 'look', 'going', 'heard', 'moment', 'came', 'last', 'take', 'half', 'love', 'room', 'pleasure', 'still', 'another', 'felt', 'something', 'sort', 'morning', 'yet', 'letter', 'saw', 'few', 'myself', 'till'];
   public lineChartColors:Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
@@ -46,10 +46,9 @@ export class BarChartDemoComponent {
   public lineChartType:string = 'line';
   public barChartHasRendered: number = 1;
 
-  public lineChartData: Array<any> =  [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  public lineChartData: Array<any> = [
+    {data: [5239, 5201, 4896, 4291, 3178, 3129, 2528, 2469, 2398, 2340, 2188, 2140, 1980, 1975, 1806, 1806, 1624, 1441, 1436, 1347, 1320, 1240, 1217, 1202, 1153, 1145, 1031, 974, 935, 865, 845, 837, 820, 759, 759, 742, 728, 699, 692, 654, 640, 600, 599, 573, 571, 570, 567, 556, 549, 546, 540, 536, 526, 506, 494, 489, 486, 485, 484, 467, 464, 455, 452, 440, 435, 432, 425, 415, 401, 398, 389, 385, 383, 371, 369, 364, 363, 359, 358, 358, 358, 352, 349, 341, 337, 326, 313, 310, 309, 306, 301, 301, 294, 284, 282, 279, 279, 264, 262, 256, 254, 250, 250, 249, 248, 241, 241, 238, 235, 230, 226, 224, 224, 222, 221, 221, 219, 217, 214, 212, 209, 208, 207, 204, 202, 200, 199, 193, 193, 193, 192, 192, 190, 178, 177, 177, 173, 172, 169, 165, 163, 161, 160, 159, 159, 155, 154, 153, 152, 148, 147, 146, 146, 146, 145, 143, 142, 142, 141, 139, 138, 138, 136, 135, 133, 132, 132, 131, 131, 130, 130, 129, 128, 125, 125, 125, 122, 121, 120, 120, 120, 119, 119, 119, 118, 117, 117, 115, 115, 114, 113, 113, 112, 111, 109, 109, 108, 106, 103, 102]
+      , label: 'Count'}
   ];
 
   public randomize(): void {
