4e015a5b8f28a70238bcb419dce7b0f07cc5b6bc
Jeremy Lim <4392084+phalax4@users.noreply.github.com>
2020-07-25 10:12:46 -0500

    Upgrade angular cli

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
-import 'chart.js/src/chart.js';
 import 'hammerjs';
 
 if (environment.production) {
